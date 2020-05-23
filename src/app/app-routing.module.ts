07cfee3705a3ae1825e97dced117ed571d01feba
paolo banez <paolo.banez@intechsive.com>
2020-05-24 01:13:04 +0800

    added shared and posts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 
 const routes: Routes = [{
   path: '',
   component: DefaultComponent,
-  children: [{
-    path: '',
-    component: DashboardComponent
-  }],
+  children: [
+    {
+      path: '',
+      component: DashboardComponent
+    },
+    {
+      path: 'posts',
+      component: PostsComponent
+    }
+  ],
 }];
 
 @NgModule({
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
 
 @NgModule({
   declarations: [
     DefaultComponent,
     DashboardComponent,
+    PostsComponent,
   ],
   imports: [
     CommonModule,
