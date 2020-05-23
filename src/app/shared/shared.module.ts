b8d31dbb1b3e843209a895237b2aada5fbfd7477
paolo banez <paolo.banez@intechsive.com>
2020-05-24 03:34:11 +0800

    added highcharts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { HighchartsChartModule } from 'highcharts-angular';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MaterialModule } from './material/material.module';
+import { AreaComponent } from './widgets/area/area.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
+    AreaComponent,
   ],
   imports: [
     CommonModule,
@@ -19,17 +22,20 @@ import { MaterialModule } from './material/material.module';
 
     MaterialModule,
     FlexLayoutModule,
+    HighchartsChartModule,
   ],
   exports: [
     // Components
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
+    AreaComponent,
 
     // Modules
+    RouterModule,
     MaterialModule,
     FlexLayoutModule,
-    RouterModule,
+    HighchartsChartModule,
   ]
 })
 export class SharedModule { }
