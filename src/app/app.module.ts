8abf8f61e00e97474eba52b7df068db0c480d8e4
paolo banez <paolo.banez@intechsive.com>
2020-05-24 00:52:00 +0800

    create default layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    DefaultModule,
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
